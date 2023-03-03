5b2ebb0996ccc936576855e30d134d8b754a738a
Felipe Romero <feliperomero3@outlook.com>
2023-03-02 22:45:15 -0600

    Creating the DbContext.

---
+++ src/MyBoardGameList/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyBoardGameList.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // TODO: custom code here
+    }
+}
