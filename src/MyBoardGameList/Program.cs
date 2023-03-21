d3dc045c4e454ce164e51e4b0073866eec16ed10
Felipe Romero <feliperomero3@outlook.com>
2023-03-20 21:45:20 -0600

    Manual Model Validation — Configuring the ApiController's behavior.

--- src/MyBoardGameList/Program.cs
+++ src/MyBoardGameList/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyBoardGameList.Data;
 using MyBoardGameList.OpenAPI;
@@ -17,6 +18,7 @@ builder.Services.AddControllers(options =>
 });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options => options.ParameterFilter<SortOrderFilter>());
+builder.Services.Configure<ApiBehaviorOptions>(options => options.SuppressModelStateInvalidFilter = true);
 
 builder.Services.AddCors(options =>
 {
