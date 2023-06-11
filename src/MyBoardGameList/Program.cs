9bdee05ba8450c7423af9fe86b8f82a425aec295
Felipe Romero <feliperomero3@outlook.com>
2023-06-10 23:43:06 -0600

    Adding a default caching directive.

--- src/MyBoardGameList/Program.cs
+++ src/MyBoardGameList/Program.cs
@@ -83,6 +83,13 @@ app.UseCors();
 
 app.UseAuthorization();
 
+app.Use((context, next) =>
+{
+    context.Response.Headers["cache-control"] = "no-cache, no-store";
+
+    return next();
+});
+
 app.MapControllers();
 
 app.Run();
