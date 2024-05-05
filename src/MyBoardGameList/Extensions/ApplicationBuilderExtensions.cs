52b0f1c4e92d0b67493c57c4268ce3daa2af995a
Felipe Romero <feliperomero3@outlook.com>
2024-05-05 00:27:25 -0600

    Extract method ConfigureExceptionHandler.

---
+++ src/MyBoardGameList/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBoardGameList.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static void ConfigureExceptionHandler(this IApplicationBuilder builder)
+    {
+        builder.Run(async context =>
+        {
+            var exceptionHandler = context.Features.Get<IExceptionHandlerPathFeature>();
+
+            var details = new ProblemDetails
+            {
+                Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1",
+                Status = StatusCodes.Status500InternalServerError,
+                Title = "An error occurred while processing your request.",
+                Detail = exceptionHandler?.Error.Message,
+                Extensions =
+                {
+                    ["traceId"] = Activity.Current?.Id ?? context.TraceIdentifier
+                }
+            };
+
+            await context.Response.WriteAsync(JsonSerializer.Serialize(details));
+        });
+    }
+}
--- src/MyBoardGameList/Program.cs
+++ src/MyBoardGameList/Program.cs
@@ -1,7 +1,3 @@
-using System.Diagnostics;
-using System.Text.Json;
-using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Net.Http.Headers;
 using MyBoardGameList.Data;
@@ -43,27 +39,7 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseExceptionHandler(action =>
-    {
-        action.Run(async context =>
-        {
-            var exceptionHandler = context.Features.Get<IExceptionHandlerPathFeature>();
-
-            var details = new ProblemDetails
-            {
-                Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1",
-                Status = StatusCodes.Status500InternalServerError,
-                Title = "An error occurred while processing your request.",
-                Detail = exceptionHandler?.Error.Message,
-                Extensions =
-                {
-                    ["traceId"] = Activity.Current?.Id ?? context.TraceIdentifier
-                }
-            };
-
-            await context.Response.WriteAsync(JsonSerializer.Serialize(details));
-        });
-    });
+    app.UseExceptionHandler(MyBoardGameList.Extensions.ApplicationBuilderExtensions.ConfigureExceptionHandler);
 }
 
 app.UseHttpsRedirection();
